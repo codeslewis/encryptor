524898fafa0aaad9ef2a9febf3aaf4ff91a08e80
Lewis Sneddon <lewis@lewiscodes.io>
2021-08-17 22:39:20 +0000

    Shift alg encryption tests

--- src/test/java/io/lewiscodes/encryptor/ShiftTest.java
+++ src/test/java/io/lewiscodes/encryptor/ShiftTest.java
@@ -13,7 +13,31 @@ public class ShiftTest {
       String result = String.valueOf(shift.encrypt(key));
       String expected = "fghi";
       assertEquals(expected, result, 
-        "Test failed for Shift encryption of standard chars: "
+        "Test failed for Shift encryption of standard chars"
+      );
+    }
+    
+    @Test
+    public void endOfCircleTest() {
+      int key = 5;
+      String testData = "uvwxy";
+      Cipher shift = new ShiftCipher(testData);
+      String result = String.valueOf(shift.encrypt(key));
+      String expected = "zabcd";
+      assertEquals(expected, result, 
+        "Test failed for Shift encryption of chars at end of circle"
+      );
+    }
+    
+    @Test
+    public void ignoresNonLetterCharsTest() {
+      int key = 5;
+      String testData = "uv w$ xy!";
+      Cipher shift = new ShiftCipher(testData);
+      String result = String.valueOf(shift.encrypt(key));
+      String expected = "za b$ cd!";
+      assertEquals(expected, result, 
+        "Test failed for Shift encryption ignoring non letter chars: " + result
       );
     }
 }
