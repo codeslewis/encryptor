cdb2d2d7605dbbbb223cca39daa9ca5cb39f222e
codeslewis <61014463+codeslewis@users.noreply.github.com>
2021-08-18 00:18:23 +0100

    Added Shift decrypt tests

--- src/test/java/io/lewiscodes/encryptor/ShiftTest.java
+++ src/test/java/io/lewiscodes/encryptor/ShiftTest.java
@@ -18,7 +18,7 @@ public class ShiftTest {
     }
     
     @Test
-    public void endOfCircleTest() {
+    public void endOfCircleEncryptionTest() {
       int key = 5;
       String testData = "uvwxy";
       Cipher shift = new ShiftCipher(testData);
@@ -30,12 +30,48 @@ public class ShiftTest {
     }
     
     @Test
-    public void ignoresNonLetterCharsTest() {
+    public void ignoresNonLetterCharsEncryptionTest() {
       int key = 5;
       String testData = "uv w$ xy!";
       Cipher shift = new ShiftCipher(testData);
       String result = String.valueOf(shift.encrypt(key));
       String expected = "za b$ cd!";
+      assertEquals(expected, result, 
+        "Test failed for Shift encryption ignoring non letter chars: " + result
+      );
+    }
+    
+    @Test
+    public void standardCharDecryptionTest() {
+      int key = 5;
+      String testData = "fghi";
+      Cipher shift = new ShiftCipher(testData);
+      String result = String.valueOf(shift.encrypt(key));
+      String expected = "abcd";
+      assertEquals(expected, result, 
+        "Test failed for Shift encryption of standard chars"
+      );
+    }
+    
+    @Test
+    public void endOfCircleDecryptionTest() {
+      int key = 5;
+      String testData = "zabcd";
+      Cipher shift = new ShiftCipher(testData);
+      String result = String.valueOf(shift.encrypt(key));
+      String expected = "uvwxy";
+      assertEquals(expected, result, 
+        "Test failed for Shift encryption of chars at end of circle"
+      );
+    }
+    
+    @Test
+    public void ignoresNonLetterCharsDecryptionTest() {
+      int key = 5;
+      String testData = "za b$ cd";
+      Cipher shift = new ShiftCipher(testData);
+      String result = String.valueOf(shift.encrypt(key));
+      String expected = "uv w$ xy!";
       assertEquals(expected, result, 
         "Test failed for Shift encryption ignoring non letter chars: " + result
       );
